aa4a11113884bed030b8c4d87c574edd7c0c41cf
Louise Cuthbertson <lcuthbertson_3112@hotmail.co.uk>
2023-02-04 11:15:53 +0000

    temporary fetch added to app.js for working purposes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,23 @@
 import './App.css'
+import {useState, useEffect} from 'react'
 
 function App() {
+
+    const [countriesData, setCountriesData] = useState([])
+
+    const getCountriesData = () => {
+        return fetch("https://restcountries.com/v3.1/all")
+            .then(res => res.json())
+    }
+
+    useEffect(() => {
+        getCountriesData()
+            .then(allCountries => {
+                setCountriesData(allCountries)
+            })
+    }, [])
+
+
     return (
         <div className="App">
         </div>
