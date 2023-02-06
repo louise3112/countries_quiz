51c76f2ad2be3690b52bf120993805f8a071942e
Maggie Amin <maggiemmagdy@gmail>
2023-02-06 09:53:49 +0000

    getting more database

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -4,13 +4,25 @@ import { getCountriesCollection } from './db_connection.js'
 const getCountries = async function () {
     return fetch ("https://restcountries.com/v3.1/all")
         .then(res => res.json())
+        .then(res => res.filter(country => country.unMember))
         .then(res => {
             return res.map(country => ({
-                name: country.name.official,
-                flag: country.flags.png,
+                name: country.name.common,
+                code: country.cca3,
+                flag: country.flags.svg,
                 population: country.population,
-                language: country.languages,
-                capital: country.capital ? country.capital[0]: null
+                language: country.languages, // array
+                capital: country.capital[0],
+                region: country.region,
+                subregion: country.subregion,
+                latitude: country.latlng[0],
+                longitude: country.latlng[1],
+                maps: country.maps.googleMaps,
+                timezones: country.timezones, //array
+                continents: country.continents, // array
+                currencies: country.currencies, // object
+                landlocked: country.landlocked,// boolean
+                borders: country.borders // array
             }))
         })
 }
