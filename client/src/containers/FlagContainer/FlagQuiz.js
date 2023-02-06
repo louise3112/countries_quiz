c7aea6db6db622b65be4521c62e2a10fee9f30e4
Gareth <g3vans16@gmail.com>
2023-02-06 15:52:34 +0000

    basic flag quiz running

--- client/src/containers/FlagContainer/FlagQuiz.js
+++ client/src/containers/FlagContainer/FlagQuiz.js
@@ -1,61 +1,95 @@
 import { randomCountries , randomIndex } from "../../helpers/usefulFunctions"
 import { getAllCountries } from "../../helpers/countryDataFetches"
-import React, { useState } from "react"
+import React, { useState , useEffect } from "react"
+import FlagsQuizList from "../../components/FlagQuizList"
 
 const FlagsQuiz = () => {
 
-    const randomCountriesArray = randomCountries(getAllCountries(), 3) // Selects 3 random country objects and puts them in an array
-    
-    const correctAnswerIndex = randomIndex(randomCountriesArray.length) // Generates a random index
+    const [answerOptions, setAnswerOptions] = useState([])
 
     // Maps over the random country array to create a new array with the data we need
-    const [answerOptions, setAnswerOptions] = useState( 
-        randomCountriesArray.map((country, index) => {
+    const prepAnswers = (countriesArray) => {
+        console.log(countriesArray)
+        const randomCountriesArray = randomCountries(countriesArray, 3) // Selects 3 random country objects and puts them in an array
+        console.log(randomCountriesArray)
+        const correctAnswerIndex = randomIndex(randomCountriesArray.length)
+        const answersList = randomCountriesArray.map((country, index) => {
             return {
-                name : country.name.common,
-                isCorrect : index === correctAnswerIndex,   // Sets 'isCorrect' to true if index matches our correct answer index, otherwise false
-                flag : country.flag.svg,
-             }
-        })
-    )
+                ...country,
+                isCorrect : index === correctAnswerIndex
+            }}
+        )
+        console.log(answersList)
+        return answersList
+    }
 
-    const correctAnswer = answerOptions.find(option => option.isCorrect)    // Assigns the correct answer object to a variable
-
-    const [userAnswer, setUserAnswer] = useState(null)
+    const [userCorrect, setUserCorrect] = useState(false)
     const [hasUserAnswered, sethasUserAnswered] = useState(false)
 
-    const handleOptionClick = (answer) => {
-        setUserAnswer(answer)
+    const processGuess = (result) => {
+        setUserCorrect(result)
         sethasUserAnswered(true)
     }
 
-      return (
+    useEffect(() => {
+        getAllCountries()
+            .then(allCountries => {
+                const selectedAnswers = prepAnswers(allCountries)
+                setAnswerOptions(selectedAnswers)
+            })
+    },[])
+
+    return (
         <div>
-          {hasUserAnswered ? (
-            <div>
-              <img src={correctAnswer.flag} alt={correctAnswer.name} />
-              {userAnswer === correctAnswer.name ? (
-                <div>Well Done!</div>
-              ) : (
-                <div>Unlucky! The correct answer is {correctAnswer.name}</div>
-              )}
-            </div>
-          ) : (
-            <div>
-              <img src={correctAnswer.flag} alt={correctAnswer.name} />
-              {answerOptions.map((option) => (
-                <button
-                  key={option.name}
-                  onClick={() => handleOptionClick(option.name)}
-                >
-                  {option.name}
-                </button>
-              ))}
-            </div>
-          )}
+            <h1>BLAH BLAH</h1>
+            <FlagsQuizList answerOptions={answerOptions} processGuess={processGuess}
+            hasUserAnswered={hasUserAnswered} userCorrect={userCorrect}  />
         </div>
-      )
+    )
+
     }
 
 
 export default FlagsQuiz
+
+
+
+
+
+
+
+
+
+    // return (
+    //     correctAnswer ? 
+    //         <img src={correctAnswer.flag} alt={correctAnswer.name} />
+    //     : 
+    //         <div>Loading...</div>
+    // )
+
+    //   return (
+    //     <div>
+    //       {hasUserAnswered ? (
+    //         <div>
+    //           <img src={correctAnswer.flag} alt={correctAnswer.name} />
+    //           {userAnswer === correctAnswer.name ? (
+    //             <div>Well Done!</div>
+    //           ) : (
+    //             <div>Unlucky! The correct answer is {correctAnswer.name}</div>
+    //           )}
+    //         </div>
+    //       ) : (
+    //         <div>
+    //           <img src={correctAnswer.flag} alt={correctAnswer.name} />
+    //           {answerOptions.map((option) => (
+    //             <button
+    //               key={option.name}
+    //               onClick={() => handleOptionClick(option.name)}
+    //             >
+    //               {option.name}
+    //             </button>
+    //           ))}
+    //         </div>
+    //       )}
+    //     </div>
+    //   )
