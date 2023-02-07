f0e7b0a7cf423dd5d49e8ecdba19a4d566ba856a
Louise Cuthbertson <lcuthbertson_3112@hotmail.co.uk>
2023-02-07 10:02:57 +0000

    minor changes

--- client/src/components/PopGameItem.js
+++ client/src/components/PopGameItem.js
@@ -10,6 +10,14 @@ const Answer = styled.p`
     margin: 0;
 `
 
+const AnswerButton = styled.button`
+    margin-left: 2em;
+    background-color: rgb(155, 162, 31);
+    color: black;
+    padding: 10px 25px;
+    border-radius: 4px;
+`
+
 const PopGameItem = ({country, processAnswer}) => {
 
     const handleClick = (evt) => {
@@ -18,13 +26,13 @@ const PopGameItem = ({country, processAnswer}) => {
 
     return (
         <div className="country-item">
-            {country.status === "current" && <button onClick={handleClick} value="higher">HIGHER</button>}
+            {country.status === "current" && <AnswerButton onClick={handleClick} value="higher">HIGHER</AnswerButton>}
             <div className="country-card">
                 <h4>{country.name}</h4>
                 <FlagPic src={country.flag} alt={"Flag for " + country.name} height={"100em"} width={"150em"}/>
                 <p><b>Population:</b> {country.status === "current" ? "????" : country.population.toLocaleString()}</p>
             </div>
-            {country.status === "current" && <button onClick={handleClick} value="lower">LOWER</button>}
+            {country.status === "current" && <AnswerButton onClick={handleClick} value="lower">LOWER</AnswerButton>}
             {country.guessCorrect && <Answer>&#9989;</Answer>}
             {country.guessCorrect === false && <Answer>&#10060;</Answer>}
         </div>
